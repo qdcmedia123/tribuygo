dbf77692f46cc10b2052122134f80b5af6890880
qdcmedia123 <info@qdc.ae>
2019-05-18 16:12:37 +0400

    Some header changed

--- application/views/can-be-less-price/templates/header.php
+++ application/views/can-be-less-price/templates/header.php
@@ -66,11 +66,9 @@
 }
 
 .section-content{
-<<<<<<< HEAD
-	
-=======
+
 	min-height: 300px;
->>>>>>> develop
+
 }
 
 
@@ -158,15 +156,15 @@ $csrf = array(
 	<div class="col-md-2">
 
 		
-<<<<<<< HEAD
+
 <h1><font color="black">tri</font>
 	<font color="red">buy</font>
 	<font color="orange">go</font>
 </h1>
-=======
+
 <h1><font color="black">tri</font><font color="red">buy</font>
 	<font color="orange">go</font></h1>
->>>>>>> develop
+
 
 	</div>
 
